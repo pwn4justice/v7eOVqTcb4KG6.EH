95ab5e6b37d4d5c61166925fe8efd013092532a6
pwn4justice <20819473@qq.com>
2019-10-23 11:06:36 -0400

    add some auto scripts

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Generate Makefile
+touch Makefile
+echo "CC = gcc" >> Makefile
+echo "" >> Makefile
+echo "main: $1.c" >> Makefile
+printf "\t\$(CC) $1.c -o main" >> Makefile
+echo "" >> Makefile
+echo "clean:" >> Makefile
+printf "\t-rm main Makefile\n" >> Makefile
+
+# Compile the given .c file
+make
+
+# Execute the binary file
+./main
+
+# After Execute, delete all files, make this directory as clean as before ;)
+make clean
+
